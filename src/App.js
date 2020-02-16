db85db1f94a2b3aa8700a326616c5f482c29c680
Kyle Chu <kchuinbox@gmail.com>
2020-02-16 17:39:23 -0500

    started nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 class App extends React.Component {
@@ -8,24 +12,48 @@ class App extends React.Component {
       title: "Kyle Chu",
       headerLinks: [
         { title: "Home", path: "/" },
-        { title: "About", path: "/" },
-        { title: "Contact", path: "/" }
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" }
       ],
       home: {
-        title: "Welcome",
-        subTitle: "Projects",
-        text: "My Projects Below"
+        title: "Be Relentless",
+        subTitle: "Projects the make a difference",
+        text: "Checkout my projects below"
       },
       about: {
         title: "About Me"
       },
       contact: {
-        title: "Contact Me?"
+        title: "Let's Talk"
       }
     };
   }
+
   render() {
-    return <h1> hello </h1>;
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Kyle Chu</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
   }
 }
 
